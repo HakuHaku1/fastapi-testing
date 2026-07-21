a6135a7aba8473606f453efd0fdafa19d39f8b5e
Ryu <acostaryu051@gmail.com>
2026-07-21 22:30:07 +0800

    editing api problem

--- __pycache__/main.py
+++ __pycache__/main.py
@@ -1,15 +1,19 @@
+
 from fastapi import FastAPI, HTTPException, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from requests import request
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
-@app.get("/",response_class=HTMLResponse)
-def root():
-    async def root(request: Request):
-        return templates.TemplateResponse("index.html", {"request": request})
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/check_password", response_class=HTMLResponse)
+async def check_password(request: Request, email: str = Form(...), password: str = Form(...)):
+    if not email or not password:
+        raise HTTPException(status_code=400, detail="Email and password are required.")
+    return templates.TemplateResponse("check_password.html", {"request": request, "email": email})
